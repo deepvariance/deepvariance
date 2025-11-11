80de0c8fa576d6849039b98281e56c35da736e2e
Saai Vignesh P <127758744+saaivignesh20@users.noreply.github.com>
2025-11-10 20:36:49 -0500

    Remove duplicate files from root directory

--- training_pipeline/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Core Training Modules
-Reusable training functions extracted from standalone scripts
-"""
-
-from .llm_training import run_llm_training
-
-__all__ = ['run_llm_training']
--- training_pipeline/strategies/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Training Strategies
-Different approaches to model training
-"""
-
-from .llm_strategy import LLMStrategy
-
-__all__ = ['LLMStrategy']
